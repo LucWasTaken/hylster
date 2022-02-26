998b8c7fb25dbe873c2fcdd88dda4573613c5c69
LucWasTaken <84717490+LucWasTaken@users.noreply.github.com>
2022-02-26 11:20:59 +0100

    files

---
+++ config.lua
@@ -0,0 +1,21 @@
+----------------------------------------------------------------
+-- Copyright © 2019 by Guy Shefer
+-- Made By: Guy293
+-- GitHub: https://github.com/Guy293
+-- Fivem Forum: https://forum.fivem.net/u/guy293/
+-- Tweaked by Campinchris
+----------------------------------------------------------------
+
+Config 				  = {} -- 850
+Config.cooldown		  = 580
+
+-- Add/remove weapon hashes here to be added for holster checks.
+Config.Weapons = {
+	"WEAPON_COMBATPISTOL",
+	"WEAPON_MACHINEPISTOL",
+	"WEAPON_PISTOL_MK2",
+	"WEAPON_FLAREGUN",
+	"WEAPON_MICROSMG",
+	"WEAPON_STUNGUN",
+	"WEAPON_APPISTOL",
+}
